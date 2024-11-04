91510228ecb4c3cb2c8296728a060ec4306b94ce
Roberto-RRF <69941000+Roberto-RRF@users.noreply.github.com>
2024-11-04 12:19:50 -0600

    added changes

--- models/pos_config.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    region = fields.Char(string="Region")
--- wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import pos_aa_sales_report
